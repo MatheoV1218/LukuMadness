e763b01109d915e535cf4545e9a63013ec082eb8
Matheo Villada <mateovillada1@outlook.com>
2026-05-20 12:59:31 -0400

    fixed issues with navbar, footer and menu. Also made it more responsive to screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import Home from "./pages/Home";
 import Story from "./pages/Story";
 import Menu from "./pages/Menu";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 import "./styles/global.css";
 
 function App() {
   return (
     <BrowserRouter>
+
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/story" element={<Story />} />
         <Route path="/menu" element={<Menu />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,62 +1,156 @@
 import { Link, useLocation } from "react-router-dom";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
+
 import "../styles/navbar.css";
 
 const Navbar = () => {
   const location = useLocation();
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNavigation = () => {
+    setMenuOpen(false);
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* LOGO */}
-        <Link to="/" className="logo">
-          <img
-            src="https://placehold.co/120x120"
-            alt="LukuMadness Logo"
-          />
-        </Link>
 
-        {/* NAV LINKS */}
-        <div className="nav-links">
+        {/* LEFT SIDE */}
+        <div className="navbar-left">
           <Link
             to="/"
-            className={location.pathname === "/" ? "active" : ""}
+            className="logo"
+            onClick={handleNavigation}
           >
-            Home
+            <img
+              src="https://placehold.co/120x120"
+              alt="LukuMadness Logo"
+            />
           </Link>
-
-          <Link
-            to="/story"
-            className={location.pathname === "/story" ? "active" : ""}
-          >
-            Our Story
-          </Link>
-
-          <Link
-            to="/menu"
-            className={location.pathname === "/menu" ? "active" : ""}
-          >
-            Menu
-          </Link>
-
-          <a
-            href="https://order.chownow.com/order/39451/locations/59964?cn_channel=cn_website"
-            target="_blank"
-          >
-            Order Online
-          </a>
         </div>
 
-        {/* SOCIALS */}
-        <div className="socials">
-          <a href="https://www.facebook.com/lukumadnessusa"
-          target="_blank">
-          
+        {/* DESKTOP NAV */}
+        <div className="navbar-right">
+
+          <div className="nav-links desktop-nav">
+            <Link
+              to="/"
+              className={location.pathname === "/" ? "active" : ""}
+              onClick={handleNavigation}
+            >
+              Home
+            </Link>
+
+            <Link
+              to="/story"
+              className={location.pathname === "/story" ? "active" : ""}
+              onClick={handleNavigation}
+            >
+              Our Story
+            </Link>
+
+            <Link
+              to="/menu"
+              className={location.pathname === "/menu" ? "active" : ""}
+              onClick={handleNavigation}
+            >
+              Menu
+            </Link>
+
+            <a
+              href="https://order.chownow.com/order/39451/locations/59964?cn_channel=cn_website"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Order Online
+            </a>
+          </div>
+
+          <div className="socials desktop-socials">
+            <a
+              href="https://www.facebook.com/lukumadnessusa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebook />
+            </a>
+
+            <a
+              href="https://www.instagram.com/lukumadness.usa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          </div>
+
+          {/* MOBILE BUTTON */}
+          <button
+            className="menu-toggle"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+      </div>
+
+      {/* MOBILE MENU */}
+      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+
+        <Link
+          to="/"
+          className={location.pathname === "/" ? "active" : ""}
+          onClick={handleNavigation}
+        >
+          Home
+        </Link>
+
+        <Link
+          to="/story"
+          className={location.pathname === "/story" ? "active" : ""}
+          onClick={handleNavigation}
+        >
+          Our Story
+        </Link>
+
+        <Link
+          to="/menu"
+          className={location.pathname === "/menu" ? "active" : ""}
+          onClick={handleNavigation}
+        >
+          Menu
+        </Link>
+
+        <a
+          href="https://order.chownow.com/order/39451/locations/59964?cn_channel=cn_website"
+          target="_blank"
+          rel="noreferrer"
+          onClick={() => setMenuOpen(false)}
+        >
+          Order Online
+        </a>
+
+        <div className="mobile-socials">
+          <a
+            href="https://www.facebook.com/lukumadnessusa"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaFacebook />
           </a>
 
-          <a href="https://www.instagram.com/lukumadness.usa"
-          target="_blank">
+          <a
+            href="https://www.instagram.com/lukumadness.usa"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaInstagram />
           </a>
         </div>
